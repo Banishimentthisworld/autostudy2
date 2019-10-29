b817333a48081d14885b6ea364a7e368ee64ebdc
LJ 神奇的 <1005620887@qq.com>
2019-10-29 11:29:53 +0800

    修改学习等待逻辑，待验证

--- test.py
+++ test.py
@@ -9,36 +9,55 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 def study(driver):
     finish_flag = 1000
-    while (finish_flag != 0):
-        try:
-            lsts = driver.find_elements_by_class_name("item-no")
-            lsts_flag = True
-        except:
-            lsts_flag = False
+    while finish_flag != 0:
+        driver.refresh()
+        time.sleep(5)
+        driver.implicitly_wait(10)
+        print("·····················梓锴牛逼·····················")
+        # 获取课程名称
+        courseTitle = driver.find_element_by_id("courseTitle")
+        print("课程标题：", courseTitle.get_attribute("title"))
+        # 获取课程章节总数
+        courses = driver.find_elements_by_class_name("cl-catalog-item")
+        print("课程章节：", len(courses))
+        # 获取以学习章节数量
+        courses_Learned = driver.find_elements_by_class_name("cl-catalog-link-done")
+        print("已学习章节：", len(courses_Learned))
+        # 获取未学习章节数量
+        courses_Learn = driver.find_elements_by_class_name("item-no")
+        print("未学习章节：", len(courses_Learn))
 
-        if lsts_flag == True:
-            finish_flag = len(lsts)
-            print(len(lsts))
-            for lst in lsts:
-                time.sleep(1)
-                driver.implicitly_wait(10)
-                lst.click()
-            # 每一分钟检查观看时间时候是否达标
-            while (int(driver.find_element_by_id("minStudyTime").text) > int(
-                    driver.find_element_by_id("studiedTime").text)):
-                time.sleep(60)
+        # 点击第一个未学习的课程
+        if len(courses_Learn) != 0:
+            courses_Learn[0].click()
+            time.sleep(5)
+            # 正在学习的章节
+            courser_learning = driver.find_element_by_class_name("cl-catalog-playing")
+            print("正在学习的章节：", courser_learning.get_attribute("title"))
+            # 每15秒检查观看时间时候是否达标
+            while int(driver.find_element_by_id("minStudyTime").text) > int(driver.find_element_by_id("studiedTime").text):
+                study_time = driver.find_element_by_id("minStudyTime").text
+                studied_time = driver.find_element_by_id("studiedTime").text
+                print("\r" + "需学习时间：" + study_time + "  已学习时间：" + studied_time, end="")
+                time.sleep(15)
 
-    while (int(driver.find_element_by_id("minStudyTime").text) > int(driver.find_element_by_id("studiedTime").text)):
-        time.sleep(60)
+        else:
+            # 完成学习
+            finish_flag = 0
+
+        print("\n·····················梓锴牛逼·····················")
+    print("所有课程已经完成学习")
 
 while(1):
     try:
         import time
         driver = webdriver.Chrome()
         driver.get("http://sunnyoptical.21tb.com/login/login.logout.do")
+        time.sleep(3)
+        driver.implicitly_wait(10)
         driver.maximize_window()
-        driver.find_element_by_xpath("//input[@id='loginName']").send_keys('1157831')
-        driver.find_element_by_xpath("//input[@id='password']").send_keys('1157831')
+        driver.find_element_by_xpath("//input[@id='loginName']").send_keys('1157830')
+        driver.find_element_by_xpath("//input[@id='password']").send_keys('1157830')
         driver.implicitly_wait(10)
         driver.find_element_by_xpath("//input[@class='btn']").click()
 
@@ -168,7 +187,6 @@ while(1):
         # 评分
         stars = driver.find_elements_by_class_name("cs-input-star")
         stars[4].click()
-
         radios = driver.find_elements_by_class_name("cs-test-radio-last")
         radios[0].click()
         radios[1].click()
@@ -176,7 +194,7 @@ while(1):
         radios[3].click()
 
         text = driver.find_element_by_class_name("cs-question-textarea")
-        text.send_keys('我觉得可以，梓锴牛逼')
+        text.send_keys('无')
         time.sleep(3)
         driver.implicitly_wait(10)
 
